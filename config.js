9cb76f0b54c0b409f3223b16d4ad4a6e486f1114
hajraata <hajraata@gmail.com>
2024-03-29 20:57:55 +0500

    first commit. MERN project

---
+++ config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+export const mongoDBURL =
+  "mongodb+srv://root:r00t@book-store-mern.n3xtwsu.mongodb.net/book-collection?retryWrites=true&w=majority&appName=Book-Store-MERN";
